8a944af7943095e711d80a6b716195f9d6b281f5
Divesh Kumar <diveshkumar94@gmail.com>
2016-09-03 17:33:14 +0530

    draw horizontal line exerciese completed

--- triangleExercises.java
+++ triangleExercises.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 class TriangleExercises {
-    public static void main( String args[] ) {
+    public static void main( String args[] ) throws Exception{
         System.out.println( "*" );
+        Scanner sc = new Scanner( System.in );
+        int n = sc.nextInt();
+        for( int i = 0; i < n; i ++ )
+            System.out.print( "*" );
+        System.out.println("");
     }
 }

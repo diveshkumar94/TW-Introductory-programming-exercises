11d1b76528f536007dc08855c84ef522cd6410ac
Divesh Kumar <diveshkumar94@gmail.com>
2016-09-03 17:52:24 +0530

    completed diamond with name and fixed issue in diamond exercise

--- diamondExercises.java
+++ diamondExercises.java
@@ -19,7 +19,25 @@ class DiamondExercises {
                 System.out.print( "*" );
             System.out.println( "" );
         }
-        for( int i = n - 1; i >= 0; i -- ) {
+        for( int i = n - 2; i >= 0; i -- ) {
+            for( int j = 0; j < n - i - 1; j ++ )
+                System.out.print( " " );
+            for( int j = 0; j <=2 * i; j ++ )
+                System.out.print( "*" );
+            System.out.println( "" );
+        }
+        n = sc.nextInt();
+        for( int i = 0; i < n - 1; i ++ ) {
+            for( int j = 0; j < n - i - 1; j ++ )
+                System.out.print( " " );
+            for( int j = 0; j <=2 * i; j ++ )
+                System.out.print( "*" );
+            System.out.println( "" );
+        }
+        for( int j = 0; j < ( 2 * n - 1 - 6 ) / 2; j ++ )
+            System.out.print( " " );
+        System.out.println( "Divesh" );
+        for( int i = n - 2; i >= 0; i -- ) {
             for( int j = 0; j < n - i - 1; j ++ )
                 System.out.print( " " );
             for( int j = 0; j <=2 * i; j ++ )
